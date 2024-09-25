28e3a9527de893e750a3ba74af39a4f19235c3a1
David Bakker <david.bakker3@hva.nl>
2024-09-25 11:55:46 +0200

    temporary navbar

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -10,8 +10,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
   ],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
