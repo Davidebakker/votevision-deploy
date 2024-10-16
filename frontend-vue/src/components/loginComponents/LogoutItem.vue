5f51c986c90a3c3fe4e3806d249d87226ee30583
David Bakker <david.bakker3@hva.nl>
2024-10-16 17:22:22 +0200

    logout fixed

--- frontend-vue/src/components/loginComponents/LogoutItem.vue
+++ frontend-vue/src/components/loginComponents/LogoutItem.vue
@@ -9,22 +9,22 @@ export default {
 
     onMounted(async () => {
       try {
-        await router.push({ name: 'login' });
-        window.location.reload();
-
-        await axios.post('http://localhost:8080/api/auth/signout', { username: 'your_username' });
+        await axios.post('http://localhost:8080/api/auth/logout', { username: 'your_username' });
 
         localStorage.removeItem('jwtToken');
         axios.defaults.headers.common['Authorization'] = null;
 
-        alert("You are successfully logged out!")
+        await router.push({ name: 'login' });
+        window.location.reload();
+
+        alert("You are successfully logged out!");
       } catch (error) {
         alert("An error occurred while logging out");
-
         console.error("Error during sign-out:", error);
       }
     });
   },
 };
+
 
 </script>
