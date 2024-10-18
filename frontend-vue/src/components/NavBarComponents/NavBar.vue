48b6e2c1616c8b02c4efeaaf1bf7f5920b683a2f
ibtisam mahamed <ibtisam.mahamed@hva.nl>
2024-10-18 13:24:27 +0200

    navbar wijziging

--- frontend-vue/src/components/NavBarComponents/NavBar.vue
+++ frontend-vue/src/components/NavBarComponents/NavBar.vue
@@ -43,14 +43,6 @@
       </router-link>
 
       <router-link
-          to="/ForumPost"
-          class="flex items-center justify-start px-4 py-2 text-gray-600 rounded-lg dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        <span class="font-medium">ForumPost</span>
-      </router-link>
-
-
-      <router-link
         v-if="!isLoggedIn"
         to="/registration"
         class="flex items-center justify-start px-4 py-2 text-gray-600 rounded-lg dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700"
