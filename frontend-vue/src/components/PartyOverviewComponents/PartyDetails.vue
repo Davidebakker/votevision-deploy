7eca05f25a6c29f37f2aedbf22f4a81e6b2dd0da
Tess Sta van Uitert <stavant@ad.hva.nl>
2024-11-12 14:48:15 +0100

    Je wordt nu doorgestuurd naar de partij waar je op klikt

--- frontend-vue/src/components/PartyOverviewComponents/PartyDetails.vue
+++ frontend-vue/src/components/PartyOverviewComponents/PartyDetails.vue
@@ -1,39 +1,36 @@
-<script setup>
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-import axios from 'axios'
-
-const route = useRoute();
-const partyName = route.params.partyName;
-const party = ref(null);
-async function fetchPartyDetails() {
-  try {
-    const response = await axios.get(`http://localhost:8080/api/elections/party/${partyName}`);
-    party.value = response.data;
-  } catch (error) {
-    console.error('Error fetching party details:', error);
-  }
-}
-
-onMounted(() => {
-  fetchPartyDetails();
-});
-</script>
-
 <template>
-  <section v-if="party" class="container mx-auto p-6">
-    <h1 class="text-2xl font-bold">{{ party.name }}</h1>
-    <img :src="party.logo" alt="Party Logo" class="w-64 h-64 my-4" />
-    <h2 class="text-lg font-semibold">Kandidaten:</h2>
-<!--    <ul>-->
-<!--      <li v-for="candidate in party.candidates" :key="candidate.candidateId">-->
-<!--        {{ candidate.name }}-->
-<!--      </li>-->
-<!--    </ul>-->
-  </section>
-  <p v-else>Loading...</p>
+  <div>
+    <h1>{{ party.name }}</h1>
+    <img :src="party.logo" alt="Party Logo" />
+    <p>Aantal zetels: {{ party.seats }}</p>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import axios from 'axios';
 
-</style>
+export default {
+  props: ['name'],
+  data() {
+    return {
+      party: {}, // Hier komen de details van de partij in
+    };
+  },
+  created() {
+    this.fetchPartyDetails();
+  },
+  methods: {
+    async fetchPartyDetails() {
+      console.log("Fetching details for party:", this.name);
+      try {
+        const response = await axios.get(`http://localhost:8080/api/elections/party/${this.name}`);
+
+        this.party = response.data;
+        console.log("Party details:", this.party); // Log de ontvangen partijgegevens
+      } catch (error) {
+        console.error("Er is een fout opgetreden bij het ophalen van de partijdetails:", error);
+      }
+    },
+  },
+};
+</script>
