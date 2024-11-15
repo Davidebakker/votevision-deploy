14a4d8535f4ce23006af40554c0f67181ee0668a
ibtisam mahamed <ibtisam.mahamed@hva.nl>
2024-11-15 11:36:46 +0100

    ongebruikte lijn, plus de andere commmits

--- frontend-vue/src/components/ForumComponents/ForumItem.vue
+++ frontend-vue/src/components/ForumComponents/ForumItem.vue
@@ -10,7 +10,6 @@ export default {
     const jwtToken = localStorage.getItem('jwtToken');
     const activeReplyCommentId = ref(null); // Track only one active reply field at a time
     const route = useRoute();
-    // const onderwerpNummer = route.params.onderwerpNummer;
 
     const fetchComments = async () => {
       try {
