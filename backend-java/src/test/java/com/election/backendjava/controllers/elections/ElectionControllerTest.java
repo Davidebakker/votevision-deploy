370eea5cfef38277094f2ff337501221e35b0c6c
David Bakker <david.bakker3@hva.nl>
2025-01-15 20:44:31 +0100

    sonarqube test merge - fix properties issue

--- backend-java/src/test/java/com/election/backendjava/controllers/elections/ElectionControllerTest.java
+++ backend-java/src/test/java/com/election/backendjava/controllers/elections/ElectionControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ElectionControllerTest {
 
