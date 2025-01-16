8d7fa2b1888a5639218ace15990c7ba24717bf3c
David Bakker <david.bakker3@hva.nl>
2025-01-16 13:36:22 +0100

    implementing sonarqube feedback - fix test issue

--- backend-java/src/test/java/com/election/backendjava/controllers/elections/ElectionControllerTest.java
+++ backend-java/src/test/java/com/election/backendjava/controllers/elections/ElectionControllerTest.java
@@ -4,11 +4,10 @@ import com.election.backendjava.controllers.ElectionController;
 import com.election.backendjava.models.election.Party;
 import com.election.backendjava.repositories.election.PartyRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +15,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ElectionControllerTest {
 
